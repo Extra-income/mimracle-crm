4b7238883be56424ac106f9ded1be6e386aae1b6
luwenbin <luwenbin@linshimuye.com>
2019-12-10 16:53:49 +0800

    添加静态文件

---
+++ mimracle/routes/artController.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const global = require('../my_modules/global');
+
+router.get('/article.html', function(req,res,next){
+  // var api = {
+  //     test: {
+  //         url: '/os/ads/getPageInfo.do',
+  //         data: {
+  //           page_code: 'OSGongXiangMeiKe'
+  //         }
+  //     }
+  // };
+
+  // global.data(req,api,function(err,resource){
+  //     var data = {
+  //         jsEntry:"VM/yishibanke/yishibanke"
+  //     };
+  //     global.formatData("一时半刻 - 林氏木业官方网站 - 用精置，活出兴致",data,req,resource);
+
+      
+  // });
+  res.render("article/index.html");
+});
+
+
+module.exports = router;
