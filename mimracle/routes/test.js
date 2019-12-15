df6b50c6cce9cfd5960687f62c0440fc22f7ae3f
luwenbin <luwenbin@linshimuye.com>
2019-12-15 18:21:19 +0800

    组装首页数据，渲染菜单栏

--- mimracle/routes/test.js
+++ mimracle/routes/test.js
@@ -2,28 +2,72 @@ const express = require('express');
 const router = express.Router();
 const global = require('../my_modules/global');
 
-router.get('/test.html', function(req,res,next){
-  var api = {
-      test: {
-          url: '/api/category/top-cagetories',
-          data: {
-            api_key: 'guangdongqiji',
-            category_id:'47',
-            page_size:'1'
-          }
-      }
-  };
+router.get('/test.html', function(req, res, next) {
 
-  global.data(req,api,function(err,resource){
-      var data = {
-          // jsEntry:"VM/yishibanke/yishibanke"
-      };
-      global.formatData("祁际网络",data,req,resource);
-      console.log('data--',data)
+    let getTopCategories = new Promise((resolve, reject) => {
+        var api = {
+            getTopCategories: {
+                url: '/api/category/top-cagetories',
+                data: {
+                    api_key: 'guangdongqiji',
+                    category_id: '47',
+                    page_size: '1'
+                }
+            }
+        };
 
-    res.render("test/index.html",data);
-      
-  });
+        global.data(req, api, function(err, resource) {
+            var data = {};
+            global.formatData("获取顶级导航栏", data, req, resource);
+            resolve(data.data);
+        });
+    });
+
+    let getAdversts = new Promise((resolve, reject) => {
+        var api = {
+            getTopCategories: {
+                url: '/api/article/sepcial-list',
+                data: {
+                    api_key: 'guangdongqiji'
+                }
+            }
+        };
+
+        global.data(req, api, function(err, resource) {
+            var data = {};
+            global.formatData("获取广告", data, req, resource);
+            resolve(data.data);
+        });
+    });
+
+    let getCustomSetting = new Promise((resolve, reject) => {
+        var api = {
+            getTopCategories: {
+                url: '/api/custom/company-setting',
+                data: {
+                    api_key: 'guangdongqiji'
+                }
+            }
+        };
+
+        global.data(req, api, function(err, resource) {
+            var data = {};
+            global.formatData("获取底部设置", data, req, resource);
+            resolve(data.data);
+        });
+    });
+
+    Promise.all([getTopCategories, getAdversts, getCustomSetting]).then((result) => {
+        let d = {
+            memus: result[0],
+            adversts: result[1],
+            customSetting: result[2]
+        };
+        console.log(d);
+        res.render("test/index.html", d);
+    }).catch((error) => {
+        console.log(error)
+    });
 });
 
 
