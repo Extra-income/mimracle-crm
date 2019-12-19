5f1b1adf140f00a96af847351f63c805443d5506
luwenbin <luwenbin@linshimuye.com>
2019-12-19 15:28:40 +0800

    添加chinaDecWeb文件夹，作公司分离

--- mimracle/my_modules/art-template/node/_node.js
+++ mimracle/my_modules/art-template/node/_node.js
@@ -1,107 +1,108 @@
 var fs = require('fs');
 var path = require('path');
 
-module.exports = function (template) {
+module.exports = function(template) {
 
-	var cacheStore = template.cache;
-	var defaults = template.defaults;
-	var rExtname;
+    var cacheStore = template.cache;
+    var defaults = template.defaults;
+    var rExtname;
 
-	// 提供新的配置字段
-	defaults.base = '';
-	defaults.extname = '.html';
-	defaults.encoding = 'utf-8';
-
-
-	// 重写引擎编译结果获取方法
-	template.get = function (filename) {
-		
-	    var fn;
-	    
-	    if (cacheStore.hasOwnProperty(filename)) {
-	        // 使用内存缓存
-	        fn = cacheStore[filename];
-	    } else {
-	        // 加载模板并编译
-	        var source = readTemplate(filename);
-	        if (typeof source === 'string') {
-	            fn = template.compile(source, {
-	                filename: filename
-	            });
-	        }
-	    }
-
-	    return fn;
-	};
-
-	
-	function readTemplate (id) {
-	    id = path.join(defaults.base, id + defaults.extname);
-	    
-	    if (id.indexOf(defaults.base) !== 0) {
-	        // 安全限制：禁止超出模板目录之外调用文件
-	        throw new Error('"' + id + '" is not in the template directory');
-	    } else {
-	        try {
-	            return fs.readFileSync(id, defaults.encoding);
-	        } catch (e) {}
-	    }
-	}
+    // 提供新的配置字段
+    defaults.base = '';
+    defaults.extname = '.html';
+    defaults.encoding = 'utf-8';
 
 
-	// 重写模板`include``语句实现方法，转换模板为绝对路径
-	template.utils.$include = function (filename, data, from) {
-	    
-	    from = path.dirname(from);
-	    filename = path.join(from, filename);
-	    
-	    return template.renderFile(filename, data);
-	}
+    // 重写引擎编译结果获取方法
+    template.get = function(filename) {
 
+        var fn;
 
-	// express support
-	template.__express = function (file, options, fn) {
-		// console.log(file,'file')
-	    if (typeof options === 'function') {
-	        fn = options;
-	        options = {};
-	    }
-
-
-		if (!rExtname) {
-			// 去掉 express 传入的路径
-			rExtname = new RegExp((defaults.extname + '$').replace(/\./g, '\\.'));
-		}
-
-
-	    file = file.replace(rExtname, '');
-
-        var theme = "";
-        if(template.theme){
-            theme = template.theme();
-            file = file.replace(options.settings.views, '');
-            if(!new RegExp("^(/|\\\\)"+theme+"(/|\\\\)").test(file)){
-                file = path.join(options.settings.views,theme,file);
-            }else{
-                file = path.join(options.settings.views,file);
+        if (cacheStore.hasOwnProperty(filename)) {
+            // 使用内存缓存
+            fn = cacheStore[filename];
+        } else {
+            // 加载模板并编译
+            var source = readTemplate(filename);
+            if (typeof source === 'string') {
+                fn = template.compile(source, {
+                    filename: filename
+                });
             }
         }
 
-        if(template.layout && template.layout() && (!options.data || !options.data["Http-X-Pjax"])){
-            var layoutfile = path.join(options.settings.views,theme,template.layout("layoutfile"));
+        return fn;
+    };
+
+
+    function readTemplate(id) {
+        id = path.join(defaults.base, id + defaults.extname);
+
+        if (id.indexOf(defaults.base) !== 0) {
+            // 安全限制：禁止超出模板目录之外调用文件
+            throw new Error('"' + id + '" is not in the template directory');
+        } else {
+            try {
+                return fs.readFileSync(id, defaults.encoding);
+            } catch (e) {}
+        }
+    }
+
+
+    // 重写模板`include``语句实现方法，转换模板为绝对路径
+    template.utils.$include = function(filename, data, from) {
+
+        from = path.dirname(from);
+        filename = path.join(from, filename);
+
+        return template.renderFile(filename, data);
+    }
+
+
+    // express support
+    template.__express = function(file, options, fn) {
+        // console.log(file,'file')
+        if (typeof options === 'function') {
+            fn = options;
+            options = {};
+        }
+
+
+        if (!rExtname) {
+            // 去掉 express 传入的路径
+            rExtname = new RegExp((defaults.extname + '$').replace(/\./g, '\\.'));
+        }
+
+
+        file = file.replace(rExtname, '');
+
+        var theme = "";
+        if (template.theme) {
+            theme = template.theme();
+            file = file.replace(options.settings.views, '');
+            if (!new RegExp("^(/|\\\\)" + theme + "(/|\\\\)").test(file)) {
+                file = path.join(options.settings.views, theme, file);
+            } else {
+                file = path.join(options.settings.views, file);
+            }
+        }
+
+        if (template.layout && template.layout() && (!options.data || !options.data["Http-X-Pjax"])) {
+            var layoutfile = path.join(options.settings.views, theme, template.layout("layoutfile"));
+            console.log("====================layoutfile", layoutfile);
             options.filename = layoutfile;
             var html_layout = template.renderFile(layoutfile, options);
             options.filename = file;
-            fn(null,  html_layout.replace("__{content}__",template.renderFile(file, options)));
-        }else{
+            fn(null, html_layout.replace("__{content}__", template.renderFile(file, options)));
+        } else {
             options.filename = file;
             fn(null, template.renderFile(file, options));
         }
 
 
 
-	};
+    };
 
 
-	return template;
+    return template;
 }
---
+++ mimracle/views/chinaDecWeb/home/tpl/index.tpl
@@ -0,0 +1,3 @@
+<script type="text/html" id="newsTpl">
+    <a></a>
+</script>
