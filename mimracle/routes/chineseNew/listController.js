7f23a43bfbde5e92893e14cc8c1221c8739c2fa8
horsemi <horsemi@outlook.com>
2019-12-22 10:59:16 +0800

    添加listcontroller 与list 模板头

--- mimracle/routes/chineseNew/listController.js
+++ mimracle/routes/chineseNew/listController.js
@@ -2,11 +2,41 @@ const express = require('express');
 const router = express.Router();
 const global = require('../../my_modules/global');
 const mimracleHelper = require('../../my_modules/mimracleHelper');
+const $ = require("../../my_modules/util");
 
-router.get('/chineseNew/list', function(req, res, next) {
+router.get('/chineseNew/list/:category_code', function(req, res, next) {
 
-    res.render("chineseNew/list/index.html");
+    let api_key = req.headers["micracle-crm"];
+    let category_key = mimracleHelper.getCatetoryKey(req.params.category_code);
+    let page_size = req.query["page_size"] || 10;
+    let page_no = req.query["page_no"] || 1;
 
+    let getArticleList = new Promise((resolve, reject ) => {
+        var api = {
+            getArticleList: {
+                url: '/api/acticle/list',
+                data: {
+                    api_key: api_key,
+                    category_key: category_key,
+                    page_size: page_size,
+                    page_no: page_no
+                }
+            }
+        };
+
+        global.data(req, api, function(err, resource) {
+            var data = {};
+            global.formatData("获取专栏文章", data, req, resource);
+            resolve(data.data);
+        })
+    });
+
+    Promise.all([getArticleList]).then((reslove) => {
+        res.render("chineseNew/list/index.html", {articleList: reslove[0]});
+        console.log(reslove);
+    }).catch((error) => {
+        $.logger.error(error);
+    });
 });
 
 module.exports = router;
