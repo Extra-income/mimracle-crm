7ac7aa050311d7ddece4704c615fa5fca9eb6b22
luwenbin <luwenbin@linshimuye.com>
2019-12-24 10:32:21 +0800

    日期格式处理

--- mimracle/my_modules/mimracleHelper.js
+++ mimracle/my_modules/mimracleHelper.js
@@ -153,6 +153,48 @@ _fn.prototype.initPagerDetail = function(pager, serachUrl) {
     return result;
 }
 
+_fn.prototype.getDefaultDateFormat = function (date) {
+    if (typeof date === "string") return date;
+    let fmt = "YYYY-mm-dd HH:MM:SS";
+    let ret;
+    let opt = {
+        "Y+": date.getFullYear().toString(),        // 年
+        "m+": (date.getMonth() + 1).toString(),     // 月
+        "d+": date.getDate().toString(),            // 日
+        "H+": date.getHours().toString(),           // 时
+        "M+": date.getMinutes().toString(),         // 分
+        "S+": date.getSeconds().toString()          // 秒
+        // 有其他格式化字符需求可以继续添加，必须转化成字符串
+    };
+    for (let k in opt) {
+        ret = new RegExp("(" + k + ")").exec(fmt);
+        if (ret) {
+            fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+        };
+    };
+    return fmt;
+}
+
+_fn.prototype.getDateFormat = function (fmt, date) {
+    let ret;
+    let opt = {
+        "Y+": date.getFullYear().toString(),        // 年
+        "m+": (date.getMonth() + 1).toString(),     // 月
+        "d+": date.getDate().toString(),            // 日
+        "H+": date.getHours().toString(),           // 时
+        "M+": date.getMinutes().toString(),         // 分
+        "S+": date.getSeconds().toString()          // 秒
+        // 有其他格式化字符需求可以继续添加，必须转化成字符串
+    };
+    for (let k in opt) {
+        ret = new RegExp("(" + k + ")").exec(fmt);
+        if (ret) {
+            fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+        };
+    };
+    return fmt;
+}
+
 let mimracleHelper = new _fn();
 
 module.exports = mimracleHelper;
